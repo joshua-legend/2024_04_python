55a28251e6fd74f76d4d11ab7db952576007abe3
Joshua <76941328+joshua-legend@users.noreply.github.com>
2024-04-09 21:41:20 +0900

    Add files via upload

---
+++ day01/input.py
@@ -0,0 +1,30 @@
+# print:글 나오도록 하는 기능
+print("안녕하세요 저는 전수효 입니다. 오늘 첫 강의 했는데 너무 좋아요")
+koreaIt = "코리아 아이티 인천점"
+print(f"당신이 공부하는 곳은 {koreaIt}")
+course = input("당신이 듣는 수업은?")
+print(f"당신이 지금 듣는 수업은 {course}군요!")
+
+# input:글을 입력 하는 기능
+# drama = input("당신의 최애 드라마는?")
+# print(f"당신의 최애 드라마는 {drama}이군요!")
+#
+# name = input("당신의 이름은?")
+# print(f"당신의 이름은 {name}이군요!")
+
+# name = input("당신의 이름?")
+# age = input("당신의 나이?")
+# color = input("당신의 좋아하는 색깔?")
+# print(f"당신의 이름은 {name} 나이는 {age} 좋아하는 색은 {color} 군요!")
+
+date = input("오늘의 날짜:")
+plan = input("할 것:")
+print(f"오늘의 날짜는 {date}, 할 계획은 {plan} 이군요!")
+
+
+
+
+
+
+
+
---
+++ day01/print.py
@@ -0,0 +1,3 @@
+print("hello world!")
+print("안녕하세요 저는 ~~~ 나이는 ~~ 살입니다.")
+print("")
