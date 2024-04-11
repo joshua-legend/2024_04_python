a1ebebed0897e717b905558e2a148c2827fcde9d
Joshua <76941328+joshua-legend@users.noreply.github.com>
2024-04-11 21:48:33 +0900

    Add files via upload

---
+++ day02/dataType.py
@@ -0,0 +1,55 @@
+# 인간: 연산[뇌] + 기억[뇌]
+# 컴퓨터: 연산[cpu] + 기억[ram] (폰노이만 구조)
+
+# 변수: RAM에 저장 하는 공간
+# 데이터 타입에 따라서 차지하는 ram공간이 다름
+# a = 2 + 3 [숫자 타입]
+# a = "안녕" [문자 타입]
+
+# 데이터 타입 종류
+a1 = 1 #정수형 타입
+a2 = 3.14 #실수형 타입
+a3 = "3.14" #문자형 타입
+a4 = True #불리언형 타입 [yes or no]
+a5 = False #불리언형 타입 [yes or no]
+a6 = None #None형 타입 [없음]
+
+#정수형,실수형 타입
+#사칙연산이 적용됨
+# b = 2 + 3.14
+# #문자형 타입
+# #연결 연산
+# c = "300" + "100"
+# #반복 연산
+# c1 = "300" * 5
+# print(c1)
+
+# print: 출력 기능
+# input: 입력 기능
+# type : 타입을 알려주는 기능
+# print(type(1))
+# print(type("1"))
+# print(type(True))
+# print(type(None))
+
+# type-casting[타입 변환]
+# 정수화: int()
+d = int("100")
+d1 = int(3.14)
+print(d1)
+
+# 실수화: float()
+e = float("3.14")
+print(e)
+
+# 문자화: str()
+f = str(True)
+print(f)
+
+# 불리언화 해주는 기능
+bool("") # True or False [0,"",None]
+
+
+
+
+
---
+++ day02/review_quiz.py
@@ -0,0 +1,16 @@
+# 1.어제의 일기 프로그램
+
+# date = input("날짜를 입력:")
+# event = input("일어 났던 일:")
+# print(f"어제는 {date}, 주요 사건은 {event}였습니다!")
+
+# 2.오늘의 음식 프로그램
+
+date = input("날짜를 입력:")
+morning = input("아침 메뉴:")
+lunch = input("점심 메뉴:")
+evening = input("저녁 메뉴:")
+print(f"오늘은 {date}, 아침에는 {morning}, 점심에는 {lunch}, 저녁에는 {evening}을 먹었습니다.")
+
+
+
